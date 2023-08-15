b771322cf57008c1825e182da38dad5a61db0f39
orel-david <42713127+orel-david@users.noreply.github.com>
2023-08-15 20:27:33 +0300

    Add game pieces defenitions
    Note that the pieces would need to interact with the board and that the pawn and knight would need special treatment

---
+++ pieces/pawn.py
@@ -0,0 +1,18 @@
+import piece
+
+
+class Pawn(piece.Piece):
+    def __init__(self, color: bool):
+        super().__init__(color)
+        self.value = 1
+        self.piece_type = piece.PieceType.PAWN
+        self.as_string = "♙" if color else "♟"
+        self.an_passant = True
+        self.start = True
+        self.moves = [(0, 1)]
+
+    def get_moves(self):
+        return self.moves if (not self.start) else (self.moves.append((0, 2)))
+
+    def can_an_passant(self):
+        return self.an_passant
---
+++ pieces/rook.py
@@ -0,0 +1,21 @@
+import piece
+
+
+class Rook(piece.Piece):
+    def __init__(self, color: bool):
+        super().__init__(color)
+        self.value = 5
+        self.piece_type = piece.PieceType.ROOK
+        self.as_string = "♖" if color else "♜"
+        self.moved = False
+        self.moves = []
+        self.moves.append((0, 1))
+        self.moves.append((1, 0))
+        self.moves.append((0, -1))
+        self.moves.append((-1, 0))
+
+    def get_moves(self):
+        return self.moves
+
+    def can_castle(self):
+        return not self.moved
