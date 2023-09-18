a5d1b43ec93128576ddf836ef7707b5732c91e8e
orel-david <42713127+orel-david@users.noreply.github.com>
2023-09-18 20:01:40 +0300

    Update binary_ops_utils.py

--- binary_ops_utils.py
+++ binary_ops_utils.py
@@ -6,8 +6,8 @@ def switch_bit(source: int, row: int, col: int, on: bool):
     :param col: The cell column
     :param on: The boolean that says whether to turn the bit on or off
     :return: The modified int
-
     """
+    
     if on:
         return source | (1 << (row * 8 + col))
     return source & (~(1 << (row * 8 + col)))
@@ -20,8 +20,8 @@ def switch_cell_bit(source: int, cell: int, on: bool):
     :param cell: The cell index
     :param on: The boolean that says whether to turn the bit on or off
     :return: The modified int
-
     """
+
     if on:
         return source | (1 << cell)
     return source & (~(1 << cell))
@@ -33,8 +33,8 @@ def translate_row_col_to_cell(row: int, col: int):
     :param row: The cell row
     :param col: The cell column
     :return: The cell index
-
     """
+
     if row > 8 or col > 8 or row < 1 or col < 1:
         return -1
     row = row - 1
@@ -47,8 +47,8 @@ def translate_cell_to_row_col(cell: int):
 
     :param cell: The cell index
     :return: A tuple of the cell row and column
-
     """
+
     return int(cell / 8), cell % 8
 
 
@@ -57,8 +57,8 @@ def get_turned_bits(word: int):
 
     :param word: The integer we inspect
     :return: A list of the indexes which their corresponding bit is on
-
     """
+
     result = []
     temp = word
     curr = 0
@@ -77,6 +77,7 @@ def get_direction(cell: int, target: int):
     :param target: The second cell
     :return: A direction value according to the directions in board or 0 if not on a line.
     """
+
     target_row, target_col = translate_cell_to_row_col(target)
     cell_row, cell_col = translate_cell_to_row_col(cell)
     row_diff = target_row - cell_row
