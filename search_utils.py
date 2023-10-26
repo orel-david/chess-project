99bc120928b9851705e7e062ee1c5cef44d9ed05
orel-david <42713127+orel-david@users.noreply.github.com>
2023-10-26 20:16:55 +0300

    Add best move search

--- search_utils.py
+++ search_utils.py
@@ -1,5 +1,6 @@
 import core
 from core import Board
+import evaluation_utils
 
 
 def count_nodes(gboard: Board, depth: int) -> int:
@@ -24,3 +25,59 @@ def count_nodes(gboard: Board, depth: int) -> int:
         sum_options += count_nodes(gboard, depth - 1)
         core.core_utils.undo_move(gboard, move)
     return sum_options
+
+def search_position(board: Board, depth: int, alpha: int, beta:int) -> float:
+    if depth <= 0:
+        return evaluation_utils.evaluate(board)
+    
+    piece_dict = board.get_pieces_dict(board.is_white)
+    moves = []
+    for piece in board.pieces_dict.values():
+        for cell in piece_dict[piece]:
+            moves += core.core_utils.get_all_legal_moves(board, cell, piece, board.is_white)
+    
+    if moves == []:
+        if board.position_in_check:
+            return float('-inf')
+        return 0
+    
+    score = 0
+    for move in moves:
+        core.core_utils.make_move(board, move, True)
+        score = -search_position(board, depth - 1, -beta, -alpha)
+        core.core_utils.undo_move(board, move)
+        
+        if score >= beta:
+            return beta
+        
+        alpha = max(alpha, score)    
+        
+    return alpha
+        
+def search_move(board : Board, depth: int) -> core.Move:
+    evaluation_utils.init_tables()
+    piece_dict = board.get_pieces_dict(board.is_white)
+    moves = []
+    
+    for piece in board.pieces_dict.values():
+        for cell in piece_dict[piece]:
+            moves += core.core_utils.get_all_legal_moves(board, cell, piece, board.is_white)
+            
+    if moves == []:
+        return None
+    
+    bestMove = None
+    bestVal = float("-inf")
+    
+    for move in moves:
+        core.core_utils.make_move(board, move, True)
+        val = -search_position(board, depth - 1, float("-inf"), float("inf"))
+        core.core_utils.undo_move(board, move)
+
+        if val >= bestVal:
+            bestMove = move
+            bestVal = val
+    
+    return bestMove
+    
+    
